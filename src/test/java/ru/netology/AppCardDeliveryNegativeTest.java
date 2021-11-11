56b7866226def2e06f7720eba09dbf201ed98564
Valentin Fadeev <valentinfs@yandex.ru>
2021-11-11 23:22:30 +0300

    Negative Tests added

--- src/test/java/ru/netology/AppCardDeliveryNegativeTest.java
+++ src/test/java/ru/netology/AppCardDeliveryNegativeTest.java
@@ -1,6 +1,8 @@
 package ru.netology;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
@@ -11,24 +13,24 @@ import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class AppCardDeliveryTest {
+public class AppCardDeliveryNegativeTest {
 
     @Test
-    void shouldTestAllValidInfo() {
+    void shouldTestAllFieldsEmpty() throws InterruptedException {
         open("http://localhost:9999");
-        $("[data-test-id='city'] [class='input__control']").setValue("Казань");
+        $("[data-test-id='city'] [class='input__control']").setValue("");
         LocalDate date = LocalDate.now();
         int year = date.getYear();
         int month = date.getMonthValue();
         int dayOfMonth = date.getDayOfMonth() + 5;
         $("[data-test-id='date'] [class='input__control']").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] [class='input__control']").setValue(dayOfMonth + "." + month + "." + year);
-        $("[data-test-id='name'] [class='input__control']").setValue("Иван Петров");
-        $("[data-test-id='phone'] [class='input__control']").setValue("+71231234567");
-        $(".checkbox__box").click();
+//        $("[data-test-id='date'] [class='input__control']").setValue(dayOfMonth + "." + month + "." + year);
+        $("[data-test-id='name'] [class='input__control']").setValue("");
+        $("[data-test-id='phone'] [class='input__control']").setValue("");
+//        $(".checkbox__box").click();
         $(withText("Забронировать")).click();
-        $(".icon_size_s").shouldBe(visible, Duration.ofSeconds(15)).click();
-
+        Thread.sleep(25000);
+//        $(".icon_size_s").shouldBe(visible, Duration.ofSeconds(15)).click();
     }
 
 }
